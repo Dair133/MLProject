ec21fb0e0f790f682a2a1ca92d247e5350c66bd1
Dair133 <114807176+Dair133@users.noreply.github.com>
2024-05-09 21:16:02 +0100

    comments

--- Primary.py
+++ Primary.py
@@ -2,6 +2,18 @@
 
 # Below is a very simple example of adjusting the weights and biases in a loop depending on the loss(difference between desired answer and calculated answer)
 
+# We are basically saying below to the model "When input is x output must be y, so adjust your weights and biases so this is the case"
+# first iteration we will not get y as the anwer when we input x, but model will attempt to fix itself by adjusting its weights and biases
+# second iteration it will be sligtly close
+# third iteration close
+# and so on 
+# eventually when we input x we will get y
+# Eventually the weights and biases will be adjusted menaing that when we input x we will get y.
+
+# This is basicallty like a very very watered down version of what happens in a real model, normally we give the model say a picture of a number between 1 and 10,
+# translated to a matrix of numbers along with the answer(we tell the model what number the picture is of)
+# The model runs the number  (represented by a matrix of numbers) through itself, compares how wrong it is and then adjusts itself accordingly
+# eventually it canr ecognise drawings of any number between 1 and 10
 
 import torch
 import torch.optim as optim
